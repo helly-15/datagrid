62f7aff5f58c59341404f1856bb90451644c9285
Lenocka <lvutina.en@gmail.com>
2020-03-04 20:10:19 +0300

    feat:add sorting

--- src/components/datagrid/Datagrid.jsx
+++ src/components/datagrid/Datagrid.jsx
@@ -1,34 +1,45 @@
 import React from 'react';
 import './dataGrid.css';
 import faker from 'faker';
+import sortTable from "../../utils/sortTable";
 
-function tableWithData (rows){
+const namesOfColumns =['avatar','name','product name', 'price', 'address','available from','email'];
+const classesOfColumns =["col-sm-1", "col-sm-2","col-sm-2","col-sm-1","col-sm-2","col-sm-1","col-sm-3"];
+
+function tableWithData (rows, columns){
+
     let table =[];
     for (let i=0;i<rows;i+=1){
         let children =[];
-            children.push (<div className="col-sm" > <img src ={faker.internet.avatar()}/> </div>)
-            children.push (<div className="col-sm" > {faker.name.firstName()} </div>)
-            children.push (<div className="col-sm" > {faker.commerce.productName()} </div>)
-            children.push (<div className="col-sm" > {faker.commerce.price()} </div>)
-            children.push (<div className="col-sm" > {`${faker.address.streetAddress()}+/n+${faker.address.city()}+/n + ${faker.address.state()}`} </div>)
-            children.push (<div className="col-sm" > {faker.commerce.productMaterial()} </div>)
-            children.push (<div className="col-sm" > <a href ={faker.internet.email()}/> </div>)
-        table.push (<div className='row' key ={i}> {children}</div> )
+        const dataInCells = {
+            "avatar": <img alt ='avatar' src ={faker.internet.avatar()}/>,
+            'name': faker.name.firstName(),
+            'product name': faker.commerce.productName(),
+            'price': faker.commerce.price(),
+            'address': faker.address.streetAddress()+' '+faker.address.city()+' '+faker.address.state(),
+            'available from':faker.date.month(),
+            'email':<a href ="" > {faker.internet.email()}</a>
+        };
+        for (let j=0; j<columns; j+=1){
+            children.push (<div className={classesOfColumns[j]} key ={i+j}> {dataInCells[namesOfColumns[j]]} </div>)
+        }
+
+        table.push (<div className='row faker-row' key ={i}> {children}</div> )
     }
     return table
 }
 
 
 export default function Datagrid (props){
-    const namesOfColumns =['Avatar','Name','Product name', 'Price', 'Product adjective','Product material','e-mail'];
+
     const {numOfRows, numOfColumns} = props;
     return(
-        <div className="container-fluid">
+        <div className="container-fluid table-wrapper">
             <>
 
                 <div className="row header-row">
                     { namesOfColumns.map((name,index)=>(
-                        <div className="col-sm" key={index+name}>
+                        <div className={classesOfColumns[index]} key={index+name} onClick = {()=>sortTable(index)}>
                             {name}
                         </div>
                     ))}
---
+++ src/utils/sortTable.js
@@ -0,0 +1,46 @@
+
+export default function sortTable(indexOfColumn) {
+
+   const table = document.getElementsByClassName("table-wrapper")[0];
+   let switching = true;
+   let dir = "asc";
+   let shouldSwitch;
+   let switchcount =0;
+   let rows = document.getElementsByClassName('faker-row');
+
+   while (switching){
+      switching =false;
+      let line =0;
+      for (let i =0; i<rows.length-1;i+=1) {
+         shouldSwitch = false;
+         let info1 = rows[i].children[indexOfColumn];
+         let info2 = rows[i + 1].children[indexOfColumn];
+         if (dir === "asc") {
+            //console.log (info1.innerHTML.toLowerCase());
+            if (info1.innerHTML.toLowerCase() > info2.innerHTML.toLowerCase()) {
+               // console.log ('switch');
+               shouldSwitch = true;
+               line = i;
+                break;
+            }
+         } else if (dir === "desc") {
+            if (info1.innerHTML.toLowerCase() < info2.innerHTML.toLowerCase()) {
+               shouldSwitch = true;
+                break;
+            }
+         }
+      }
+         if (shouldSwitch){
+            rows[line].parentNode.insertBefore(rows[line + 1], rows[line]);
+            switching = true;
+            switchcount ++;
+         }
+         else {
+            if (switchcount === 0 && dir === "asc") {
+               dir = "desc";
+               switching = true;
+            }
+         }
+
+   }
+}
